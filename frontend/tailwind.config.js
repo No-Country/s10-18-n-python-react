f7cf3575cabb796a9698c5ae3588973510a6b481
Gonzalo <garciacalvog@hotmail.com>
2023-08-24 13:31:15 -0300

    componente Hero con responsive, fonts locales

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'elipse': "url('src/assets/images/elipse.jpg')",
+      },
+      borderRadius: {
+        'elipse': '50%'
+      },
+      colors: {
+        'greenBrand': '#128983'
+      },
+      fontFamily: {
+        baloo2: ["Baloo2", "sans-serif"],
+        latoRegular: ["Lato-regular", "sans-serif"],
+        latoMedium: ["Lato-Medium", "sans-serif"],
+        latoSemibold: ["Lato-Semibold", "sans-serif"],
+        latoBold: ["Lato-Bold", "sans-serif"]
+      },
+    }
   },
   plugins: [],
 }
