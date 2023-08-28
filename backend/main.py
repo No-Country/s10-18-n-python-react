0c39802c0a25c4a0e6c1156b8298d28d566df277
hectordsol <hectordsol@gmail.com>
2023-08-27 23:38:27 -0300

    status code

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Response
+from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from models.user_connection import UserConnection
 from schema.user_schema import UserSchema
 
 app = FastAPI()
 conn = UserConnection()
 
-@app.get('/users')
+@app.get('/users', status_code=HTTP_200_OK)
 async def raiz():
      items=[]
      for data in conn.read_all():
@@ -17,22 +18,16 @@ async def raiz():
           items.append(dictionary)
      return items
 
-@app.post("/users/")
+@app.post("/users/", status_code=HTTP_201_CREATED)
 async def create_user(user: UserSchema):
     data=user.dict()
     data.pop("id")
     print(data)
     conn.write(data)
-    return {'nuevo'}
+    return Response(status_code=HTTP_201_CREATED)
 
-@app.put("/users/{id}")
-async def update_one(user: UserSchema, id:str):
-    data=user.dict()
-    data["id"] = id
-    conn.update_one(data)
-    return {'updated'}
 
-@app.get("/users/{id}")
+@app.get("/users/{id}", status_code=HTTP_200_OK)
 async def get_one(id: str):
      dictionary = {}
      data = conn.read_one(id)
@@ -42,6 +37,13 @@ async def get_one(id: str):
      dictionary["domicilio"] = data[3]
      return data
 
-@app.delete("/users/{id}")
+@app.put("/users/{id}", status_code=HTTP_204_NO_CONTENT)
+async def update_one(user: UserSchema, id:str):
+    data=user.dict()
+    data["id"] = id
+    conn.update_one(data)
+    return {'updated'}
+
+@app.delete("/users/{id}", status_code=HTTP_204_NO_CONTENT)
 async def delete_one(id: str):
      conn.delete_one(id)
