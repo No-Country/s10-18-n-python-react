c7409d8ad45f8e163ecd3f34a071448481a8a679
NicoHub98 <78396936+NicoHub98@users.noreply.github.com>
2023-09-07 14:38:19 -0300

    small fix addEventModal

--- frontend/src/components/DatosPaciente.jsx
+++ frontend/src/components/DatosPaciente.jsx
@@ -1,66 +1,133 @@
-import logo from "../assets/logo1.png"
-import calendar from "../assets/calendar.png"
+import logo from "../assets/logo1.png";
+import calendar from "../assets/calendar.png";
+import { Input, InputGroup, SelectPicker } from "rsuite";
+import { useState } from "react";
+import axios from "axios";
 
-const DatosPaciente = () => {
-    return (
-        <>
-        <div className="w-full h-full flex justify-center">
-            <div className="w-[800px] h-[786px] pl-24 pr-24 ">
-                <div className="flex flex-row items-center  ">
-                    <div className="">
-                        <img src={logo} alt="" />
-                    </div>
-                    <p>Sanatorio integral de salud</p>
-                </div>
+const DatosPaciente = ({ onClose, start }) => {
+  const URL =
+    "http://ec2-3-17-60-17.us-east-2.compute.amazonaws.com:8000/appointments/";
+  const [name, setName] = useState("");
+  const [dni, setDni] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [socialWork, setSocialWork] = useState("");
 
-                <div className="flex flex-row mt-16">
-                    <div className="w-full h-full">
-                        <h2 className="text-left ml-14">Datos del paciente</h2>
-                        <hr className="h-1 bg-[#712EFF] rounded-md"/>
-                    </div>
-                </div>
+  console.log(start);
 
-                <div className="flex flex-row text-left gap-12 mt-6 ">
-                    <div className="">
-                        <p>Fecha de nacimiento</p>
-                        <img src={calendar} alt="calendario" className="absolute mt-4 ml-[210px]"/>
-                        <input type="text" placeholder="3/03/2016" className="bg-[#F0F0F0] rounded-md w-[256px] h-12"/>
-                    </div>
+  const dataSocialWork = ["Obstetricia", "Pediatría", "Oftalmología"].map(
+    (item) => ({ label: item, value: item })
+  );
 
-                    <div className="">
-                        <p>DNI</p>
-                        <input type="text" placeholder="3488654" className="bg-[#F0F0F0] rounded-md w-[256px] h-12 "/>
-                    </div>
-                </div>
-
-                <div className="text-left">
-                    <div className="mt-5 ">
-                        <p>Nombre y Apellido</p>
-                        <input type="text" placeholder="Nuñez, Thiago" className="w-full h-12 bg-[#f0f0f0] rounded-md"/>
-                    </div>
-                    <div className="mt-5">
-                        <p>Telefono/mail de contacto</p>
-                        <input type="text" placeholder="2244- 15789906" className="w-full h-12 bg-[#f0f0f0] rounded-md"/>
-                    </div>
-                    <div className="mt-5">
-                        <p>Nombre y Apellido</p>
-                        <input type="text" placeholder="Osmiss" className="w-full h-12 bg-[#f0f0f0] rounded-md"/>
-                    </div>
-                </div>
-
-                <div >
-                    <div className="w-full h-full">
-                        <div className="flex justify-end mt-20 gap-4">
-                            <button className="border-1 border-black bg-[#fff]">Cancelar</button>
-                            <button className="bg-[#DC4928]">Agendar</button>
-                        </div>
-                    </div>
-                </div>
-
+  const postAppointment = async (appointmentNewData) => {
+    const request = await axios.post(
+      URL,
+      new URLSearchParams(appointmentNewData),
+      {
+        headers: { accept: "application/json" },
+      }
+    );
+    const response = await request.data.user;
+    localStorage.setItem("user", JSON.stringify(response));
+    toast.success("Has iniciado sesión correctamente");
+    return response;
+  };
+  return (
+    <>
+      <div className="w-full h-full flex justify-center">
+        <div className="   ">
+          <div className="flex flex-row items-center  ">
+            <div className="">
+              <img src={logo} alt="logo" />
             </div>
-        </div>
-        </>
-    )
-}
+            <p>Sanatorio integral de salud</p>
+          </div>
 
-export default DatosPaciente
+          <div className="flex flex-row mt-10">
+            <div className="w-full h-full">
+              <h2 className="text-left ml-14">Datos del paciente</h2>
+              <hr className="h-1 bg-[#712EFF] rounded-md" />
+            </div>
+          </div>
+
+          <div className="flex flex-col sm:flex-row text-left gap-5 sm:gap-10 mt-6">
+            <div className="">
+              <p>Fecha de nacimiento</p>
+              <InputGroup>
+                <Input
+                  autoFocus
+                  className="bg-[#F0F0F0]"
+                  placeholder="03/10/2023"
+                  onChange={(e) => setBirthDate(e)}
+                  value={birthDate}
+                />
+                <InputGroup.Addon>
+                  <img src={calendar} alt="calendario" />
+                </InputGroup.Addon>
+              </InputGroup>
+            </div>
+
+            <div className="">
+              <p>DNI</p>
+              <Input
+                value={dni}
+                onChange={(e) => setDni(e)}
+                className="bg-[#F0F0F0]"
+                placeholder="3488654"
+              />
+            </div>
+          </div>
+
+          <div className="text-left">
+            <div className="mt-5 ">
+              <p>Nombre y Apellido</p>
+              <Input
+                value={name}
+                onChange={(e) => setName(e)}
+                className="bg-[#F0F0F0]"
+                placeholder="Nuñez Thiago"
+              />
+            </div>
+            <div className="mt-5">
+              <p>Telefono/mail de contacto</p>
+              <Input
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e)}
+                type="number"
+                className="bg-[#F0F0F0]"
+                placeholder="2244- 15789906"
+              />
+            </div>
+            <div className="mt-5">
+              <p>Obra Social</p>
+              <SelectPicker
+                data={dataSocialWork}
+                className="bg-[#F0F0F0]"
+                style={{ width: 224 }}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="w-full h-full">
+              <div className="flex justify-end mt-20 gap-4">
+                <button
+                  onClick={onClose}
+                  className="border border-black border-solid rounded-lg py-2 px-4 bg-white hover:scale-95 transition-all text-black"
+                >
+                  Cancelar
+                </button>
+                {/* className="border-1 border-black bg-[#fff]" */}
+                <button className="border border-orange border-solid rounded-lg py-2 px-4 hover:scale-95 transition-all bg-orange font-latoRegular text-black">
+                  Agendar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DatosPaciente;
--- frontend/src/components/bigCalendar/AddEventModal.jsx
+++ frontend/src/components/bigCalendar/AddEventModal.jsx
@@ -1,43 +1,49 @@
-import { Modal, Button } from 'rsuite';
-import 'rsuite/dist/rsuite.min.css';
-import logo from "../../assets/logo1.png"
-import DatosPaciente from '../DatosPaciente';
+import { Modal, Button } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
+import logo from "../../assets/logo1.png";
+import DatosPaciente from "../DatosPaciente";
+import { useEffect, useState } from "react";
 
-const AddEventModal = ({event, openAdd, handleClose}) => {
+const AddEventModal = ({ event, openAdd, handleClose }) => {
   /* const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false); */
 
   //console.log('event en EventModal: ',event )
+  const [startDate, setStartDate] = useState(0);
 
-  if (event) {
-   const start =new Date(event.start)
-   const startHours = start.getHours()
-   const startMinutes = start.getMinutes().toString().padStart(2, "0")
-   const end =new Date(event.end)
-   const endHours = end.getHours()
-   const endMinutes = end.getMinutes().toString().padStart(2, "0")
-  }
+  useEffect(() => {
+    if (event) {
+      const start = new Date(event.start);
+      setStartDate(start);
+      const startHours = start.getHours();
+      const startMinutes = start.getMinutes().toString().padStart(2, "0");
+      const end = new Date(event.end);
+      const endHours = end.getHours();
+      const endMinutes = end.getMinutes().toString().padStart(2, "0");
+    }
+  }, []);
+
   /* console.log("startMinutes: ", startMinutes) */
   return (
     <>
       {/* <ButtonToolbar>
         <Button onClick={handleOpen}> Open</Button>
       </ButtonToolbar> */}
-      <Modal 
-        backdrop={true} 
-        open={openAdd} 
-        onClose={handleClose} 
+      <Modal
+        backdrop={true}
+        open={openAdd}
+        onClose={handleClose}
         overflow={true}
-        size={'md'}
-        dialogClassName='p-0 rounded-none '
+        size={"md"}
+        dialogClassName="p-0 rounded-none "
         /* className='p-8 bg-blue-700 rounded-none' */
       >
-        <div className='p-12 border-[2em] border-[#3E36B0] font-baloo2  BANDERA '>
-        <DatosPaciente />
+        <div className="p-12 border-[2em] border-[#3E36B0] font-baloo2  BANDERA ">
+          <DatosPaciente onClose={handleClose} start={startDate} />
         </div>
       </Modal>
     </>
   );
 };
-export default AddEventModal
+export default AddEventModal;
